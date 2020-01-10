ca846ded78dee628843154e441fe31c5442c3f68
Rodrigo Trejo <rigotre7@gmail.com>
2020-01-09 22:25:55 -0500

    Step 1: Calculate the total price of oranges and apples based on provided requirements

--- src/main/ShoppingCart.java
+++ src/main/ShoppingCart.java
@@ -1,5 +1,47 @@
 package main;
 
-public class ShoppingCart {
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
 
+public class ShoppingCart {
+	
+	private static final String APPLE_PRICE = ".6";
+	private static final String ORANGE_PRICE = ".25";
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter list of apples and oranges split by commas (\",\").");
+	
+		// Get the user's input into an array of strings
+		String userInput = scanner.nextLine();
+		String[] groceryArr = userInput.split(",");
+				
+		NumberFormat formatter = NumberFormat.getCurrencyInstance(Locale.UK);
+		System.out.println(formatter.format(calculatePrice(groceryArr)));
+		
+		scanner.close();
+	}
+	
+	/**
+	 * Calculates the total price of the items in the grocery array
+	 * @param groceryItems - Array of grocery items
+	 * @return total price of items
+	 */
+	private static BigDecimal calculatePrice(String[] groceryItems) {
+		
+		BigDecimal total = BigDecimal.ZERO;
+		
+		// Aggregate the price in a simple loop
+		for (String item : groceryItems) {
+			if (item.toLowerCase().trim().equals("apple")) {
+				total = total.add(new BigDecimal(APPLE_PRICE));
+			} else if (item.toLowerCase().trim().equals("orange")) {
+				total = total.add(new BigDecimal(ORANGE_PRICE));
+			}
+		}
+		
+		return total;
+	}
 }
